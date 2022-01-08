01391437895dc5454a443c0940fe6f247a9cf0ec
Najam Syed <najam.r.syed@gmail.com>
2022-01-08 12:08:15 -0500

    Use list directly instead of range

--- leetcode/300_longest_increasing_subsequence/300.py
+++ leetcode/300_longest_increasing_subsequence/300.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1 for _ in range(len(nums))]
+        dp = [1 for _ in nums)]
         
         for i in range(1, len(nums)):
             for j in range(i):
